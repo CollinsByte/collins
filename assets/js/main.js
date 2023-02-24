2a461011e6fb0c05f62cecc1ed2053490682f27f
colspan <collin@collindigm.com>
2023-02-24 13:18:09 +0300

    JavaScript: Prototype of the page

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// Grab elements
+
+// Nav Styles on scroll
+
+// Open menu & search pop-up
+
+// Open/Close Search form pop-up
+
+// -- Close the search form pop-up on ESC keypress
+
+// Switch theme/add to local storage
+
+// Swiper
