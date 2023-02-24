91e8ca5ec3dabb2f20bb246c8d35e8134fbedee8
colspan <collin@collindigm.com>
2023-02-24 13:23:30 +0300

    JavaScript: Custom function to grab elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 // Grab elements
+const selectElement = (selector) => {
+    const element = document.querySelector(selector);
+    if (element) {
+        return element;
+    }
+
+    throw new Error(`Something went wrong, make sure that ${selector} exists or is typed correctly.`);
+};
 
 // Nav Styles on scroll
 
