d3d52ec02c7c005619f9ed5383ce57c98cf8ae36
colspan <collin@collindigm.com>
2023-02-25 21:25:35 +0300

    JS: Added Event Listener to toggle the dark and ligth theme body

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,5 +36,11 @@ menuToggleIcon.addEventListener("click", toggleMenu);
 // -- Close the search form pop-up on ESC keypress
 
 // Switch theme/add to local storage
+const bodyElement = document.body;
+const themeToggleBtn = selectElement("#theme-toggle-btn");
+
+themeToggleBtn.addEventListener("click", () => {
+    bodyElement.classList.toggle("light-theme");
+});
 
 // Swiper
