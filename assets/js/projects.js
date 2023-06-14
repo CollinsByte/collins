74f4dd1095e65db72e97e4df32e979a18d50708d
colspan <collin@collindigm.com>
2023-06-14 08:30:35 +0300

    displayProfile function: Display information from github profile

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -24,7 +24,29 @@ const getProfile = async() => {
     console.log(`awaiting the response from the fetch request`);
     const profile = await response.json();
     console.log(profile);
+    displayProfile(profile)
 };
 
-
 getProfile();
+
+// display information from github profile 
+const displayProfile = (profile) => {
+    const userInfo = selectElement('.user-info');
+    userInfo.innerHTML = `
+        <figure>
+            <img alt="user avatar" src=${profile.avatar_url} />
+        </figure>
+        <div>
+            <h2><a href=${profile.blog}><strong>${profile.name}</strong></h2>
+            <p>${profile.bio}</p>
+            <p>
+                <strong>Location:</strong> ${profile.location}
+            </p>
+            <p>
+                <strong>@${profile.login}</strong>
+                Repos: ${profile.public_repos}
+                Gists: ${profile.public_gists}
+            </p>
+        </div>
+    `;
+};
